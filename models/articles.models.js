28e9b0310ea2b95b82c66d0c5de2d0f2b8723684
Chris-Lupton <kitlupton@hotmail.com>
2023-08-14 17:27:20 +0100

    completes GET /api/articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -7,3 +7,14 @@ exports.fetchArticleById = async (id) => {
     }
     return rows[0]
 }
+
+exports.fetchArticle = async () => {
+    const { rows } = await db.query(`
+            SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id) AS comment_count
+            FROM articles
+            LEFT JOIN comments ON articles.article_id = comments.article_id
+            GROUP BY articles.article_id
+            ORDER BY articles.created_at DESC
+    `)
+    return rows
+}
